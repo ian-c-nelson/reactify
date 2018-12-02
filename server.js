eb67b4de8a032093b8f35798225fbdeadadedd90
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2018-12-01 18:01:01 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("app/build"));
+
+app.listen(PORT, function(){
+    console.log(`App listening on PORT ${PORT}`);
+});
