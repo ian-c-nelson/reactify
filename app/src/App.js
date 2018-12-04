30e29ee1c6239a37f1274b49fd980096e8d802a2
tuckerbeauchamp <tuckerbeauchamp@gmail.com>
2018-12-04 16:31:39 -0700

    add redux

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import {Provider} from "react-redux";
-// import {createStore} from "redux"
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 import Navbar from "./components/common/Navbar";
+
+import rootReducer from "./reducers";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <Router>
-          <Switch>
-            <Route path="/" exact component={Hello} />
-            <Route path="/test" exact component={Test} />
-          </Switch>
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Navbar />
+          <Router>
+            <Switch>
+              <Route path="/" exact component={Hello} />
+              <Route path="/test" exact component={Test} />
+            </Switch>
+          </Router>
+        </div>
+      </Provider>
     );
   }
 }
 
 function Hello() {
-  return <p>Hello!</p>
-};
+  return <p>Hello!</p>;
+}
 
 function Test() {
-  return <p>Test!</p>
-};
+  return <p>Test!</p>;
+}
 
 export default App;
